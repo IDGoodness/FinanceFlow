9ff13daafaa9972e46b442de2254073d7a3598d7
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-12-02 14:18:54 +0100

    Resolved issues with 'TestimonialCard.jsx' causing deployment to fail.

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import TestimonialCard from "./testimonialCard";
+import TestimonialCard from "./TestimonialCard";
 import { testimonialCardData } from "../../testimonialCardData";
 
 const Carousel = () => {
